356fe349fd1de526b8fc6c26f1e21311a208e4f2
MZen2610 <mzen1980@gmail.com>
2021-01-01 18:45:05 +0300

    Add frontend

--- where_to_go/posters/views.py
+++ where_to_go/posters/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
 
 def home(request):
-    context = {
-    }
-    return render(request, template_name='posters/home.html', context=context)
+    return render(request, template_name='base.html')
