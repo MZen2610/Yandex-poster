8a5cee202a9c81394e3509fbc0fb10f2d7f2f86f
MZen2610 <mzen1980@gmail.com>
2021-01-07 13:45:35 +0300

    Move data from template to view

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -3,16 +3,10 @@ from django.db import models
 
 class Place(models.Model):
     title = models.CharField(max_length=150, verbose_name='Наименование')
-    imgs = models.ForeignKey('Images', on_delete=models.PROTECT, null=True,
-                             verbose_name='Изображения', blank=True)
-    description_short = models.TextField(blank=True, verbose_name='Краткое '
-                                                                  'описание')
-    description_long = models.TextField(blank=True, verbose_name='Полное '
-                                                                 'описание')
-    lng = models.DecimalField(max_digits=17, decimal_places=14, \
-                              verbose_name='Долгота')
-    lat = models.DecimalField(max_digits=17, decimal_places=14,
-                              verbose_name='Широта')
+    description_short = models.TextField(blank=True, verbose_name='Краткое описание')
+    description_long = models.TextField(blank=True, verbose_name='Полное описание')
+    lng = models.DecimalField(max_digits=17, decimal_places=14, verbose_name='Долгота')
+    lat = models.DecimalField(max_digits=17, decimal_places=14, verbose_name='Широта')
 
     def __str__(self):
         return self.title
@@ -24,10 +18,9 @@ class Place(models.Model):
 
 
 class Images(models.Model):
-    title = models.CharField(max_length=150, verbose_name='Наименование')
+    title = models.ForeignKey('Place', on_delete=models.SET_NULL, null=True, verbose_name='Место', blank=False)
     num = models.IntegerField(verbose_name='Номер')
-    image = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True,
-                              verbose_name='Изображение')
+    image = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True, verbose_name='Изображение')
 
     def __str__(self):
         return f"{self.num} {self.title}"
--- where_to_go/places/views.py
+++ where_to_go/places/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from .models import Place, Images
+import json
 
-# Create your views here.
+
+def home(request):
+    places = Place.objects.all()
+    imgs = Images.objects.filter()
+
+    places_geojson = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        37.62,
+                        55.793676
+                    ]
+                },
+                "properties": {
+                    "title": "«Легенды Москвы",
+                    "placeId": "moscow_legends",
+                    "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        37.64,
+                        55.753676
+                    ]
+                },
+                "properties": {
+                    "title": "Крыши24.рф",
+                    "placeId": "roofs24",
+                    "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/roofs24.json"
+                }
+            }
+        ]
+    }
+
+    # print('places_geojson = ', type(places_geojson))
+
+    context = {
+        'places_geojson': places_geojson,
+        # 'title': 'Hello !!!'
+    }
+    # return render(request, template_name='places/home.html', context=context)
+    return render(request, template_name='base.html', context=context)
