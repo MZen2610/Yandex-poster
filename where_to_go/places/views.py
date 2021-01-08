743af5950cc34fe77cadcbd9b5ae53801c1c8995
MZen2610 <mzen1980@gmail.com>
2021-01-08 12:58:34 +0300

    Updated places/views, outputs json data

--- where_to_go/places/views.py
+++ where_to_go/places/views.py
@@ -1,53 +1,39 @@
 from django.shortcuts import render
 from .models import Place, Images
-import json
 
 
 def home(request):
     places = Place.objects.all()
     imgs = Images.objects.filter()
 
-    places_geojson = {
-        "type": "FeatureCollection",
-        "features": [
+    features = []
+
+    for item in places:
+        features.append(
             {
                 "type": "Feature",
                 "geometry": {
                     "type": "Point",
                     "coordinates": [
-                        37.62,
-                        55.793676
+                        item.lng,
+                        item.lat
                     ]
                 },
                 "properties": {
-                    "title": "«Легенды Москвы",
-                    "placeId": "moscow_legends",
+                    "title": item.title,
+                    "placeId": item.id,
                     "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json"
                 }
-            },
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [
-                        37.64,
-                        55.753676
-                    ]
-                },
-                "properties": {
-                    "title": "Крыши24.рф",
-                    "placeId": "roofs24",
-                    "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/roofs24.json"
-                }
             }
-        ]
-    }
+        )
 
-    # print('places_geojson = ', type(places_geojson))
+    places_geojson = {
+        "type": "FeatureCollection",
+        "features": features
+    }
 
     context = {
         'places_geojson': places_geojson,
-        # 'title': 'Hello !!!'
     }
-    # return render(request, template_name='places/home.html', context=context)
+
     return render(request, template_name='base.html', context=context)
